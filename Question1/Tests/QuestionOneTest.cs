3ad8d240fb0ebea14edbc860bb63fda7535a1080
Ori Maci <djorion317@gmail.com>
2017-02-12 11:46:11 -0500

    Additional testing for Question One.

--- Question1/Tests/QuestionOneTest.cs
+++ Question1/Tests/QuestionOneTest.cs
@@ -43,5 +43,41 @@ namespace Question1.Tests
 
             Assert.AreEqual(expectedMergedLength, compute.SortedArray.Length);
         }
+
+        [Test]
+        public void MergeTwoValidArraysAndCheckIfSortedCorrectly()
+        {
+            // Default constructor
+            ArrayComputation compute = new ArrayComputation();
+
+            // Two arrays of the same size
+            int[] a = { 100, 23, 454, 6, 7, 8 };
+            int[] b = { 1, 222, 43, 2, 56, 8 };
+
+            Assert.IsNotNull(a.Length, "The input array has not been initialized yet");
+
+            int expectedMergedLength = a.Length * 2;
+            compute.mergeArray(a, b, a.Length);
+
+            // Let's compare all values are ordered from least to greatest
+            var sortedArray = compute.SortedArray;
+            for (int i = 0; i <= sortedArray.Length; i++)
+            {
+                var leftNode = sortedArray.ElementAt(i);
+
+                // We are at the end of the array, let's break out 
+                // so we don't throw an exception
+                if (i == sortedArray.Length - 1)
+                {
+                    break;
+                }
+
+                var rightNode = sortedArray.ElementAt(++i);
+                if (leftNode > rightNode)
+                {
+                    Assert.Fail("The array is not properly sorted!");
+                }
+            }
+        }
     }
 }
